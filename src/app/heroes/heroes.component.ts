1d983f281b7e2fea20ca54db38562eb741e7931a
wholespace <wholespace.214@gmail.com>
2022-03-02 00:17:54 +0900

    Add Navigation

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from "../hero";
 import { HeroService } from "../hero.service";
-import { MessageService } from "../message.service";
 
 @Component({
   selector: 'app-heroes',
@@ -9,11 +9,9 @@ import { MessageService } from "../message.service";
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
   heroes: Hero[] = [];
-  selectedHero?: Hero;
 
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
     this.getHeroes();
@@ -23,10 +21,4 @@ export class HeroesComponent implements OnInit {
     this.heroService.getHeroes()
     .subscribe(heroes => this.heroes = heroes);
   }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
-  }
-
 }
